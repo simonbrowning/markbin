e9ee8703c9e391228af71027ac0db025c5a90081
Simon Browning <simonbrowning86@gmail.com>
2017-05-19 09:04:35 +0100

    Initial Commit
    Completed project

---
+++ client/components/header.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Link, browserHistory } from 'react-router';
+
+import Accounts from './accounts';
+
+class Header extends Component {
+  onBinClick(event) {
+    event.preventDefault();
+
+    Meteor.call('bins.insert',(err, binId) => {
+      browserHistory.push(`/bins/${binId}`);
+    });
+  }
+
+  render() {
+    return (
+      <nav className="nav navbar-default">
+        <div className="navbar-header">
+          <Link to="/" className="navbar-brand">Markbin</Link>
+        </div>
+        <ul className="nav navbar-nav">
+          <li>
+            <Accounts />
+          </li>
+          <li>
+            <a href="#" onClick={this.onBinClick.bind(this)}>Create Bin</a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Header;
